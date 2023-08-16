5171116813d9fd105819ff6fe0d301a490aef8c5
RenderCoder <RenderCoder@sot.app>
2023-08-16 16:13:55 +0800

    fix: use original useState

--- src/hooks/useMount.ts
+++ src/hooks/useMount.ts
@@ -1,25 +1,3 @@
-import { useEffect, useRef } from 'react';
-import type { EffectCallback } from 'react';
-import { isSSR } from '../utils/isSSR';
+import { useEffect } from 'react';
 
-function useMountForClient(callback: EffectCallback) {
-  const mounted = useRef(false);
-  useEffect(() => {
-    let clearAction: () => void | void;
-    if (!mounted.current) {
-      mounted.current = true;
-      const result = callback();
-      if (result) {
-        clearAction = result;
-      }
-    }
-
-    return () => {
-      if (clearAction) {
-        clearAction();
-      }
-    };
-  }, [mounted]); // eslint-disable-line react-hooks/exhaustive-deps
-}
-
-export const useMount = isSSR ? useEffect : useMountForClient;
+export const useMount = useEffect;
